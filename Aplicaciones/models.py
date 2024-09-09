6da866ffd9c6658b8b2b01c825efe80a51c726ba
Sahid-Morales-5500 <sahidm2023@gmail.com>
2024-09-09 11:53:40 -0500

    Creacion del Modelo de Base de Datos

--- Aplicaciones/models.py
+++ Aplicaciones/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    Titulo = models.CharField(max_length=200)
+    Descripcion = models.TextField()
+    Genero = models.CharField(max_length = 30)
+    ID = models.CharField(max_length=3)
+
